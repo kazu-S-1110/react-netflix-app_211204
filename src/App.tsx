b1d5ba5569747008c9b48689aa2abc87226ef20f
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-12-07 08:07:11 +0900

    Bannerコンポーネントを実装。

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Banner } from './Banner';
 import { requests } from './request';
 import { Row } from './Row';
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
