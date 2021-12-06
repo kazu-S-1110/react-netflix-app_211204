5e24bf247f4446228dedb900248c28b407f98ab4
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-12-07 06:16:06 +0900

    Fetchができていなかったとこを英語ドキュメントを見ながら修正。

--- src/request.js
+++ src/request.js
@@ -4,9 +4,9 @@ export const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/discover/tv?api_key=${API_KEY}&language=en-us`,
-  fetchActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=28`,
+  fetchActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=Action`,
   fetchComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=27`,
+  fetchHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=Horror`,
   fetchRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
   fetchDocumentMovies: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
 };
