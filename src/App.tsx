2ef7c786682a487c74f009fca925e09a5c5bce4e
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-12-07 12:29:39 +0900

    Navバーを実装。これが自分でできるようになりたいな〜。

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Banner } from './Banner';
+import { Nav } from './Nav';
 import { requests } from './request';
 import { Row } from './Row';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
